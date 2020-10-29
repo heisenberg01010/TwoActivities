425c1a5d6d571f534e05a1c5651b6e1157ba8178
Heisenberg01010 <54992414+heisenberg01010@users.noreply.github.com>
2020-10-29 16:16:02 +0530

    view binding approach

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -5,26 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import com.example.twoactivities.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
-    private EditText mReply;
-
+    ActivitySecondBinding b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
+        b = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+        b.textMessage.setText(message);
     }
 
     public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+        String reply = b.editTextSecond.getText().toString();
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
